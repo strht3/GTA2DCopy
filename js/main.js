9f9f015878daf59ca8c6da4aba28078466134518
strht3 <strht3@gmail.com>
2025-01-28 16:21:11 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -788,7 +788,7 @@ mainScene.punchBeam = function(direction){
         }
     }else if(this.attacktype === 'Bat'){
         punchdelay = 2000;
-        punch.setDisplaySize(300,300)
+        punch.setDisplaySize(200,200)
         if(direction == 'right'){
             punch.setAngle(90);
             punch.anims.play('Explosion',true);
