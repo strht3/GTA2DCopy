e74cfb25087e5c40b880a33865b14b937ea5c4d9
strht3 <strht3@gmail.com>
2025-01-28 16:39:41 +0900

    Size change

--- js/main.js
+++ js/main.js
@@ -788,8 +788,8 @@ mainScene.punchBeam = function(direction){
         }
     }else if(this.attacktype === 'Bat'){
         punchdelay = 2000;
-        punch.setDisplaySize(300,300)
-        punch.setSize(300,300);
+        punch.setDisplaySize(600,600)
+        punch.setSize(600,600);
         if(direction == 'right'){
             punch.setAngle(90);
             punch.anims.play('Explosion',true);
